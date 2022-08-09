d1e622cc8c089c6af4becc10881108bb49ea2acd
ArjunChainani <arjun.k.chainani@gmail.com>
2022-08-09 11:28:23 -0400

    completed

--- init-firebase.js
+++ init-firebase.js
@@ -3,8 +3,14 @@
  * provided for you by firebase after creating your project!
 */
 const firebaseConfig = {
-    
-};
+    apiKey: "AIzaSyAHsROXxwLVhZlZZp12dXfKF-UHZiw0RMA",
+    authDomain: "fire-message-f6d06.firebaseapp.com",
+    projectId: "fire-message-f6d06",
+    storageBucket: "fire-message-f6d06.appspot.com",
+    messagingSenderId: "1095580247961",
+    appId: "1:1095580247961:web:d180ef10246d6eab559508",
+    measurementId: "G-TR0HJ2LSR2"
+  };
 
 // initialize the firebase app with this built-in function
 firebase.initializeApp(firebaseConfig);
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /**
  * @TODO get a reference to the Firebase Database object
  */
+const database = firebase.database().ref();
 
 /**
  * @TODO get const references to the following elements:
@@ -10,6 +11,12 @@
  *      - button with id #send-btn and the updateDB
  *        function as an onclick event handler
  */
+const allMessages = document.getElementById('all-messages');
+const usernameInput = document.getElementById('username');
+const messageInput = document.getElementById('message');
+const sendButton = document.getElementById('send-btn');
+
+sendButton.onclick = updateDB;
 
 /**
  * @TODO create a function called updateDB which takes
@@ -21,12 +28,28 @@
  *      - writes this object to the database
  *      - resets the value of #message input element
  */
+function updateDB(event) {
+    // prevents the page from refreshing
+    event.preventDefault();
+
+    // create an object with the data we want to add to our database
+    const row = {
+        USERNAME: usernameInput.value,
+        MESSAGE: messageInput.value
+    };
+
+    console.log(row);
+
+    database.push(row);
+}
 
 /**
  * @TODO add the addMessageToBoard function as an event
  * handler for the "child_added" event on the database
  * object
  */
+// called one time for each row in database on page load and each entry
+database.on('child_added', addMessage);
 
 /**
  * @TODO create a function called addMessageToBoard that
@@ -38,6 +61,14 @@
  *        #all-messages (we should have a reference already!)
  * 
  */
+function addMessage(rowData) {
+    // gets the value in the database row
+    const messageObject = rowData.val();
+    console.log(messageObject);
+
+    let messageDiv = makeSingleMessageHTML(messageObject.USERNAME, messageObject.MESSAGE);
+    allMessages.appendChild(messageDiv);
+}
 
 /** 
  * @TODO create a function called makeSingleMessageHTML which takes
@@ -56,6 +87,24 @@
  * 
  *      - returns the parent div
  */
+function makeSingleMessageHTML(usernameTxt, messageTxt) {
+    // creates a parent div to hold entire username + message line
+    let parentDiv = document.createElement('div');
+    parentDiv.classList.add('single-message');
+
+    let usernameDisplay = document.createElement('p');
+    usernameDisplay.classList.add('single-message-username');
+    // update the inner HTML to include the username
+    usernameDisplay.innerHTML = usernameTxt + ':';
+    parentDiv.appendChild(usernameDisplay);
+
+    // adds the message
+    let messageDisplay = document.createElement('p');
+    messageDisplay.innerHTML = messageTxt;  
+    parentDiv.appendChild(messageDisplay);
+
+    return parentDiv;
+}
 
 /**
  * @BONUS add an onkeyup event handler to the form HTML
